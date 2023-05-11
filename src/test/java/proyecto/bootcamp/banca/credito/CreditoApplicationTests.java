762c340813b671e7f5af0a972eaed90e3b0e95f9
dnns <dennis.ah@gmail.com>
2023-05-11 16:57:03 -0500

    Proyecto base credito

---
+++ src/test/java/proyecto/bootcamp/banca/credito/CreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.bootcamp.banca.credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
